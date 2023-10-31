771f55aa2d0a9b7493582755c81c2b886f77b6d0
indriabernade <83918486+indriabernade@users.noreply.github.com>
2023-10-31 15:59:33 +0700

    Update connectHost.dart

--- lib/connection/connectHost.dart
+++ lib/connection/connectHost.dart
@@ -1,5 +1,4 @@
 class Connection {
-  static const hostConnection = "https://l2c.oss.myrepublic.co.id/tik/";
   static const hostConnectionUser = "$hostConnection/user/";
 
   static const register = "$hostConnectionUser/register.php";
@@ -10,9 +9,4 @@ class Connection {
   static const deleteData = "$hostConnectionUser/delete.php";
   static const notice = "$hostConnectionUser/notify.php";
   static const deletenotice = "$hostConnectionUser/deletenotify.php";
-
-  static const myrepData = "https://apicore.myrepublic.net.id/tyk/apis/";
-  static const myrepReload =
-      "https://apicore.myrepublic.net.id/tyk/reload/group";
-  static const myrepKeys = "https://apicore.myrepublic.net.id/tyk/keys/create";
 }
